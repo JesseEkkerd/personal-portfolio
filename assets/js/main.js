5a281643c391da70e9a025a323893ee886e3c0b5
Jesse Ekkerd <jesseekkerd@gmail.com>
2022-06-17 16:48:45 +0200

    Add testimonials section

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,32 @@ $(document).ready(function () {
   });
 });
 /* Hamburger Animation End */
+
+/* Fade on Scroll */
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.5,
+  // rootMargin: "0px 0px 60px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+/* Fade on Scroll End */
